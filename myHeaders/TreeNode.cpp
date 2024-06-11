65410ec73be4eed1c2094bec9b71c96b6a5d1b86
jaemin-han <duddjakgkrl@gmail.com>
2024-06-11 13:14:01 +0900

    TreeNode header

---
+++ myHeaders/TreeNode.cpp
@@ -0,0 +1,64 @@
+#include "TreeNode.h"
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+TreeNode *createBinaryTree(const vector<int> &nums)
+{
+    if (nums.empty())
+        return nullptr;
+
+    queue<TreeNode *> q;
+    TreeNode *root = new TreeNode(nums[0]);
+    q.push(root);
+
+    size_t i = 1;
+    while (i < nums.size())
+    {
+        TreeNode *current = q.front();
+        q.pop();
+
+        if (i < nums.size() && nums[i] != numeric_limits<int>::min())
+        {
+            current->left = new TreeNode(nums[i]);
+            q.push(current->left);
+        }
+        i++;
+
+        if (i < nums.size() && nums[i] != numeric_limits<int>::min())
+        {
+            current->right = new TreeNode(nums[i]);
+            q.push(current->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+// Utility function to print the tree in level order (for testing purposes)
+void printTreeLevelOrder(TreeNode *root)
+{
+    if (!root)
+        return;
+
+    queue<TreeNode *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        TreeNode *current = q.front();
+        q.pop();
+        if (current)
+        {
+            cout << current->val << " ";
+            q.push(current->left);
+            q.push(current->right);
+        }
+        else
+        {
+            cout << "null ";
+        }
+    }
+    cout << endl;
+}
---
+++ myHeaders/TreeNode.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <vector>
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode *createBinaryTree(const std::vector<int> &nums);
+void printTreeLevelOrder(TreeNode *root);
+
+const int NULL_NODE = std::numeric_limits<int>::min();
