4d1b59298cb607d210febb09cdcb795aa751d9a9
jaemin-han <duddjakgkrl@gmail.com>
2024-07-12 19:49:07 +0900

    feat: Add Convert1DArrayInto2DArray solution

---
+++ Convert1DArrayInto2DArray/Convert1DArrayInto2DArray.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<vector<int>> construct2DArray(vector<int> &original, int m, int n)
+    {
+    }
+};
+
+int main()
+{
+    return 0;
+}
