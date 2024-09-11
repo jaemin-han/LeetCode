376e5473345ef5b9a6f8cbcbbb81d7c5e0d90d66
jaemin-han <duddjakgkrl@gmail.com>
2024-09-11 21:09:15 +0900

    solved.

--- AverageofLevelsinBinaryTree/AverageofLevelsinBinaryTree.cpp
+++ AverageofLevelsinBinaryTree/AverageofLevelsinBinaryTree.cpp
@@ -24,24 +24,40 @@ public:
 
         while (!myQueue.empty())
         {
-            if (count == 0)
-                sum = 0;
-
             auto curPair = myQueue.front();
             myQueue.pop();
 
-            if (curPair. && )
+            if (curPair.second > curLevel)
+            {
+                curLevel = curPair.second;
+                answer.push_back(double(sum) / count);
+                sum = curPair.first->val;
+                count = 1;
+            }
+            else
+            {
+                sum += curPair.first->val;
+                count++;
+            }
 
-            
+            if (curPair.first->left)
+                myQueue.push({curPair.first->left, curPair.second + 1});
+
+            if (curPair.first->right)
+                myQueue.push({curPair.first->right, curPair.second + 1});
         }
+        answer.push_back(double(sum) / count);
+        return answer;
     }
 };
 
 int main()
 {
 
-    auto *root = createBinaryTree({3, 9, 20, NULL_NODE, NULL_NODE, 15, 7});
+    // auto *root = createBinaryTree({3, 9, 20, NULL_NODE, NULL_NODE, 15, 7});
     // auto* root = createBinaryTree({3,9,20,15,7});
+    // auto* root = createBinaryTree({3});
+    auto* root = createBinaryTree({2147483647, 2147483647, 2147483647});
 
     Solution sol;
 
---
+++ MaximumAverageSubarray1/MaximumAverageSubarray1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        
+    }
+};
